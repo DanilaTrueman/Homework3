2b6eadc49a81ee54e3bde538ae8e008cfa2b074f
DanilaTrueman <148095014+DanilaTrueman@users.noreply.github.com>
2023-11-06 01:18:25 +0300

    new commit PrimeNumbers

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -11,21 +11,20 @@ public class PrimeNumbers {
 
         boolean isPrime = true;
 
-        if (number <= 1) {
+        if (number < 2) {
             isPrime = false;
         } else {
-
-            for (int i =  2; i <= Math.sqrt(number);
-            i++) {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0){
                     isPrime = false;
                     break;
                 }
             }
         }
-        if (isPrime) {
+        if (number == 0 || number == 1) {
+            System.out.println(number + " не является ни простым, ни составным числом");
+        } else if (isPrime) {
             System.out.println(number + " является простым числом");
-
         } else {
             System.out.println(number + " не является простым числом");
         }
